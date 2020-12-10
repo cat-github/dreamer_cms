088ab5adb9f1597afdab5cb1d5ce79efbcba1f57
王俊南 <153095904@qq.com>
2020-12-10 15:03:52 +0800

    开发后台横版编辑功能。主题上传不需要main.xml文件改为json文件。

---
+++ src/main/java/cn/itechyou/cms/vo/TemplateVo.java
@@ -0,0 +1,32 @@
+package cn.itechyou.cms.vo;
+
+public class TemplateVo {
+	private String path;
+	private String file;
+	private String content;
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getFile() {
+		return file;
+	}
+
+	public void setFile(String file) {
+		this.file = file;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
